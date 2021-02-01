c17ae9dcdf9a72d38490c40692358ab3ffe337a3
Manuel Mejia <bxprdr25@hotmail.com>
2021-01-31 21:49:54 -0500

    Complete ERD, Schema and Queries

---
+++ EmployeeSQL/EmployeeSQL_queries.sql
@@ -0,0 +1,64 @@
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
+FROM "Employees" AS e
+JOIN "Salaries" AS s
+ON e.emp_no = s.emp_no
+ORDER BY e.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM "Employees" 
+WHERE hire_date >= '1986-01-01' AND hire_date < '1987-01-01'
+ORDER BY hire_date DESC;
+
+--3. List the manager of each department with the following information: department number, department name, 
+-- the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, dm.emp_no AS manager_emp_no, e.first_name AS manager_first_name, e.last_name AS manager_last_name
+FROM "Departments" AS d
+JOIN "Dept_Manager" AS dm
+ON d.dept_no = dm.dept_no
+JOIN "Employees" AS e
+ON dm.emp_no = e.emp_no;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT de.emp_no, e.first_name, e.last_name, d.dept_name
+FROM "Dept_Emp" AS de
+JOIN "Employees" AS e
+ON de.emp_no = e.emp_no
+JOIN "Departments" AS d
+ON de.dept_no = d.dept_no
+ORDER BY emp_no;
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name
+FROM "Employees"
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.first_name, e.last_name, d.dept_name
+FROM "Dept_Emp" AS de
+JOIN "Employees" AS e
+ON de.emp_no = e.emp_no
+JOIN "Departments" AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+ORDER BY emp_no;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, e.first_name, e.last_name, d.dept_name
+FROM "Dept_Emp" AS de
+JOIN "Employees" AS e
+ON de.emp_no = e.emp_no
+JOIN "Departments" AS d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' 
+OR d.dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(last_name) AS "frequency"
+FROM "Employees"
+GROUP BY last_name
+ORDER BY
+COUNT(last_name) DESC;
---
+++ EmployeeSQL/EmployeeSQL_schema.sql
@@ -0,0 +1,81 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/AfoewZ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+--Create Table Statements
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(255)   NOT NULL,
+    "dept_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(255)   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(255)   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(255)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "gender" varchar   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar(255)   NOT NULL,
+    "title" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+--Import data from CSV's using the import feature of PGADMIN.  Import must occur first before applying the Foreign Key (FK) constraints
+
+--Add FK Constraints
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+--Confirm that all data imported successfully after applying the FK constraints
+
+SELECT * FROM "Dept_Emp";
+SELECT * FROM "Departments";
+SELECT * FROM "Dept_Manager";
+SELECT * FROM "Employees";
+SELECT * FROM "Salaries";
+SELECT * FROM "Titles";
